9636dbb96bee263e91c0dafd17c4bcb8b3e28e62
Kyoshiro laptotdev <tahir.gregory@gmail.com>
2023-07-08 22:33:10 +0200

    Revert "panneau admin en fr"
    This reverts commit c13184300dd5c8d6d6ff492463b258257f7d4cd0.

--- src/Controller/Admin/PrestationsCrudController.php
+++ src/Controller/Admin/PrestationsCrudController.php
@@ -4,7 +4,6 @@ namespace App\Controller\Admin;
 
 use App\Entity\Prestations;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-
 class PrestationsCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
