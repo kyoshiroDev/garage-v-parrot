d69e9258aebd39bf060edf3dd527b7207a812458
Kyoshiro laptotdev <tahir.gregory@gmail.com>
2023-07-08 15:39:30 +0200

    database

--- src/Controller/HoraireController.php
+++ src/Controller/HoraireController.php
@@ -13,7 +13,19 @@ class HoraireController extends AbstractController
   #[Route('/horaire', name: 'app_horaire')]
   public function horaire(HoraireRepository $repository): Response
   {
+
     $horaires = $repository->findAll();
+
+    $form = $this->createForm(HoraireType::class, $horaires);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+    
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($horaires);
+      $entityManager->flush();
+
+      return $this->redirectToRoute('app_horaire');
+    }
 
     return $this->render('_part/horaire/index.html.twig', [
       'horaires' => $horaires,
